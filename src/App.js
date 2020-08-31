07a49723027ffd02f79d446239eb5e8b979383a0
pimonwan.sutmee <pimonwan.sutmee@scb.co.th>
2020-08-31 17:17:41 +0700

    basic render

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  //render method use for render something to screen
   render() {
     return (
+      //className = add css class to element
+      //Jsx must have 1 root element (now div is root element)
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi I'm a React App </h1>
+        <p> This is really working!!</p>
       </div>
     );
+
+    //==============================//
+    //Another way to write screen
+    //Use function createElement that need at least 3 arguments
+
+    // 1 element to render
+    // 2 configuration like styles
+    // 3 children in element (mean what inside div)
+
+    // return React.createElement(
+    //   'div', 
+    //   {className: 'App'}, 
+    //   React.createElement('h1', null, 'Hi I\'m a React App!!')
+    // );
+
+     //==============================//
   }
 }
 
