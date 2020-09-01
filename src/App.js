a9bec3387b91606ee56d79db150f2e534701b264
pimonwan.sutmee <pimonwan.sutmee@scb.co.th>
2020-09-01 15:42:07 +0700

    Using the useState() Hook for State Manipulation.

--- src/App.js
+++ src/App.js
@@ -1,75 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-//import component from person file
 import Person from './Person/Person.js';
 
-class App extends Component {
-  // state is variables in class
-  state = {
+const app = props => {
+  /* useState is the hook that allows us to manage state in a functional
+   - useState can pass all types like objects, numbers, strings etc.
+   - useState always return 2 arrays 
+   - The first is current state and the second always be a function that allow to update state
+
+  // ******* important ******* //
+   useState will be replace new state to the old state 
+   example : 
+     - before changing state
+             persons: [
+              {name: 'Max', age: 28},
+              {name: 'Manu', age: 29},
+              {name: 'Stephanie', age: 26}
+            ],
+            otherState: 'hi'
+            });
+    - after changing state
+          persons: [
+            {name: 'Maximilian', age: 28},
+            {name: 'Manu', age: 29},
+            {name: 'Stephanie', age: 27}
+          ]
+          });
+    otherState will be lost.
+
+    So to manage state we can create another useState function to manage otherState seperate with personsState
+
+    ** setState function will merge data between old state and new state so state will not lost
+
+  */
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
-    ]
-  }
+    ],
+    otherState: 'hi'
+    });
 
-  switchNameHandler = () => {
-    //Don't do this ---> this.state.persons[0].name = 'Maximilian';
+  const [otherState, setOtherState] = useState('some other value');
+  console.log(personsState, otherState);
 
-    // setState allows us to update state property
-    // get attribute type object
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
       {name: 'Maximilian', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 27}
     ]
-    })
+    });
   }
-  //render method use for render something to screen
-  render() {
-    return (
-      //className = add css class to element
-      //Jsx must have 1 root element (now div is root element)
-      //Person is component that can reuse.
-    //==============================//
-    // in html we can pass attributes in <elements attributes>
-    //   <div className="App">
-    //     <h1>Hi I'm a React App </h1>
-    //     <p> This is really working!!</p>
-    //     <Person name='Max' age='28'/>
-    //     <Person name='Manu' age='29'>My Hobbies: Racing</Person>
-    //     <Person name='Stephanie' age='26'/>
-    //   </div>
-    // );
-
-    //==============================//
-    //Another way to write screen
-    //Use function createElement that need at least 3 arguments
-
-    // 1 element to render
-    // 2 configuration like styles
-    // 3 children in element (mean what inside div)
-
-    // return React.createElement(
-    //   'div', 
-    //   {className: 'App'}, 
-    //   React.createElement('h1', null, 'Hi I\'m a React App!!')
-    // );
-     //==============================//
-    // pass attribute by using state
-      <div className="App">
-        <h1>Hi I'm a React App </h1>
-        <p> This is really working!!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
+  return (
+    <div className="App">
+      <h1>Hi I'm a React App </h1>
+      <p> This is really working!!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age}/>
+      <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+      <Person 
+        name={personsState.persons[2].name} 
+        age={personsState.persons[2].age}/>
+    </div>
     );
-    //==============================//
-  }
 }
 
-export default App;
+export default app;
