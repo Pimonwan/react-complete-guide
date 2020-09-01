5f0700acec64046e27d56f0f9138a61b5fdde637
pimonwan.sutmee <pimonwan.sutmee@scb.co.th>
2020-09-01 15:06:57 +0700

    onClick function for action when press button.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,27 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  // state is variables in class
   state = {
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
     ]
+  }
+
+  switchNameHandler = () => {
+    //Don't do this ---> this.state.persons[0].name = 'Maximilian';
+
+    // setState allows us to update state property
+    // get attribute type object
+    this.setState({
+      persons: [
+      {name: 'Maximilian', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 27}
+    ]
+    })
   }
   //render method use for render something to screen
   render() {
@@ -47,6 +62,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a React App </h1>
         <p> This is really working!!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
