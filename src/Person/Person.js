a4427f8c5b47cb85303cd7534053489022a682c7
pimonwan.sutmee <pimonwan.sutmee@scb.co.th>
2020-09-02 21:48:46 +0700

    create Person.css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}> I'm a {props.name}! and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
 
 export default person;
+
+/*
+<input type="text" onChange={props.changed} value={props.name}/>
+ - element is text field
+ - onChange is action when it changes
+ - value is default value in text field
+
+ ** if use 'value' without 'onchange' handler it will always show error in console
+ */
