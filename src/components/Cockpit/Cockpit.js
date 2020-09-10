ca881e9831452cd50b25ae29b09bfd7c793aec9f
pimonwan.sutmee <pimonwan.sutmee@scb.co.th>
2020-09-10 15:56:00 +0700

    add useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Cockpit.module.css";
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+
+  // useEffect if array is [] it will run 1 times after rendering
+  // [props.Person] it will run when persons have changed.
+  // if need componentDidMount, you would use useEffect with an empty array passed as a second.
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    //HTTP Request... 
+    setTimeout(() => {
+      alert('Save data to cloud!!')
+    }, 1000);
+  }, [props.persons]);
+
+
+
   const assignedClasses = [];
   let btnClasses = "";
   if (props.showPersons) {
@@ -25,4 +39,4 @@ const cockpit = (props) => {
   );
 };
 
-export default cockpit;
+export default Cockpit;
